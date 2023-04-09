212dda0b029530b8350b633ed97754153a84d6db
mgcollie <mgcollie@gmail.com>
2023-04-09 17:25:12 -0400

    Create main.py
    INITIAL COMMIT

---
+++ main.py
@@ -0,0 +1,79 @@
+import os
+import openai
+import pyttsx3
+import speech_recognition as sr
+from typing import Optional
+
+engine = pyttsx3.init()
+
+recognizer = sr.Recognizer()
+openai.api_key = os.getenv('OPEN_API_KEY')
+
+
+def text_to_speech(text: str) -> None:
+    print(text)
+    engine.say(text)
+    engine.runAndWait()
+
+
+def call_openai(prompt: str, temperature: float = 0.5, max_tokens: int = 100) -> str:
+    prompt = prompt.strip()
+
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=messages,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        n=1,
+        stop=None,
+    )
+    return response.choices[0].message.content.strip()
+
+
+def listen_and_transcribe(prompt: str) -> Optional[str]:
+    text_to_speech(prompt)
+
+    with sr.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source, duration=1)  # Added this line
+        print("Listening...")
+        audio = recognizer.listen(source)
+
+    try:
+        print("Transcribing...")
+        text = recognizer.recognize_google(audio)
+        return text
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand the audio")
+        return None
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        return None
+
+
+def main() -> int:
+    prompt = "How many tokens do you want to use?"
+    response = listen_and_transcribe(prompt)
+    if response:
+        try:
+            max_tokens = int(response)
+            print('You said: ', max_tokens)
+        except ValueError:
+            print("Invalid input. Please provide an integer value.")
+            return -1
+    else:
+        return -1
+
+    prompt = "What is your question?"
+    question = listen_and_transcribe(prompt)
+    if question:
+        print('You said: ', question)
+        data = call_openai(question, max_tokens=max_tokens)
+        text_to_speech(data)
+        return 0
+    else:
+        return -1
+    
+    
+if __name__ == "__main__":
+    main()
